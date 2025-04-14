6e9789493311d85559e183e5ba190a38d40d2bc9
Amaan Khan <amaanmkhan2@gmail.com>
2025-04-13 19:16:12 -0500

    Add predeployment config to simulate vercel linting locally

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // These match Vercel's stricter rules
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "off", // Use the TypeScript rule instead
+      "import/no-extraneous-dependencies": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
